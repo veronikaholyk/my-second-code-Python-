23ffbac5054fe0d004247c01d66fa0270d1007ca
veronikaholyk <veronika.holyk8@gmail.com>
2026-05-29 08:37:39 +0300

    Create aibot.py

---
+++ aibot.py
@@ -0,0 +1,10 @@
+print("Hello.I'm your first AI-assistant")
+name = input("What's your name? ")
+
+print("Nice to meet you, " + name + "!")
+mood = input("How are u today? (write: good or bad): ")
+
+if mood.lower() == "good":
+    print("Perfect!")
+else:
+    print("It's okay, everythink will be good")
